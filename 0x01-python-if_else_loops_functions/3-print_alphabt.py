ffad8dfb612e35efcac3ee45dd6ce181c5565fcd
Abdulla Omar <aabdula2712@gmail.com>
2024-07-15 19:45:55 +0300

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == 113:
-        continue
-    elif i == 101:
-        continue
-    else:
-        print("{}".format(chr(i)), end="")
-	
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")	
