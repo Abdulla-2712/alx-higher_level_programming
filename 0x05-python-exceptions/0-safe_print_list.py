d9d8fdf52a0572c7b68ff8633b283f1354151281
Abdulla Omar <aabdula2712@gmail.com>
2024-07-22 21:35:05 +0300

    Ddd

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         while count < x:
-            print("{}".format(my_list[count]), end=" ")
+            print("{:d}".format(my_list[count]), end=" ")
             count += 1
         print()  # Print a newline after the loop
     except IndexError:
