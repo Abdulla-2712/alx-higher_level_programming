e00bc8fca1b1f2dbb1aa69627a973bbdc689d0cc
Abdulla Omar <aabdula2712@gmail.com>
2024-07-23 00:01:11 +0300

    SS

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if str(abs(value)).isdigit():
-            print("{:d}".format(value))
-            return True
-        else:
-            raise Exception
+        print("{:d}".format(value))
+        return True
+
     except TypeError:
         return False
