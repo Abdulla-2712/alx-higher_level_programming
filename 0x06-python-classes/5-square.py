e7c3739ea713c856b232383f3bcf750a09770370
Abdulla Omar <aabdula2712@gmail.com>
2024-07-23 19:04:06 +0300

    .

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -33,7 +33,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
-            print()
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
